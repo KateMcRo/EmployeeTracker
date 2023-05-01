9f02fda20ea5dfe0d56ea46c24356c43e65f70d5
KateMcRo <katemcr0@gmail.com>
2023-04-30 18:21:24 -0700

    updated all table displays

--- queries/index.js
+++ queries/index.js
@@ -1,6 +1,26 @@
 const GET_ALL_DEPARTMENTS = "SELECT * FROM departments"
-const GET_ALL_ROLES = "SELECT * FROM roles"
-const GET_ALL_EMPLOYEES = "SELECT * FROM employees"
+
+const GET_ALL_ROLES = 
+`SELECT roles.title AS job_title,
+roles.id AS role_id,
+departments.name AS department_name,
+roles.salary AS role_salary
+FROM roles
+JOIN departments ON roles.department_id = departments.id;`
+
+const GET_ALL_EMPLOYEES = 
+`SELECT 
+employees.id, 
+CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name,
+roles.title AS title, 
+departments.name AS department,
+roles.salary AS salary,
+CONCAT_WS(' ', manager.first_name, manager.last_name) AS manager
+FROM employees 
+JOIN roles ON employees.role_id = roles.id 
+JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees AS manager ON employees.manager_id = manager.id;
+`
 
 const ADD_DEPARTMENT = "INSERT INTO departments SET ?"
 
